087335e5779773e00054a634138d42c280c368ab
Frederik Elwert <frederik.elwert@web.de>
2010-11-27 23:15:47 +0100

    Implement skip to

---
+++ transcribe/SkiptoDialog.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+import ctypes
+import datetime
+
+import gtk
+
+from transcribe.helpers import get_builder, ns_to_time, time_to_ns, trim
+
+import gettext
+from gettext import gettext as _
+gettext.textdomain('transcribe')
+
+class SkiptoDialog(gtk.Dialog):
+    __gtype_name__ = "SkiptoDialog"
+
+    def __new__(cls):
+        """Special static method that's automatically called by Python when 
+        constructing a new instance of this class.
+        
+        Returns a fully instantiated SkiptoDialog object.
+        """
+        builder = get_builder('SkiptoDialog')
+        new_object = builder.get_object('skipto_dialog')
+        new_object.finish_initializing(builder)
+        return new_object
+
+    def finish_initializing(self, builder):
+        """Called when we're finished initializing.
+
+        finish_initalizing should be called after parsing the ui definition
+        and creating a SkiptoDialog object with it in order to
+        finish initializing the start of the new SkiptoDialog
+        instance.
+        """
+        # Get a reference to the builder and set up the signals.
+        self.builder = builder
+        self.builder.connect_signals(self)
+        self.time_format = '%M:%S.%f'
+        self.spinbutton = self.builder.get_object('spinbutton_time')
+        
+    def on_spinbutton_time_input(self, spinbutton, value_ptr):
+        text = spinbutton.get_text()
+        try:
+            time = datetime.datetime.strptime(text, self.time_format).time()
+        except ValueError:
+            return -1
+        double = ctypes.c_double.from_address(hash(value_ptr))
+        double.value = float(time_to_ns(time))
+        return True
+        
+    def on_spinbutton_time_output(self, spinbutton):
+        time = ns_to_time(int(spinbutton.get_value()))
+        text = trim(time.strftime(self.time_format))
+        spinbutton.set_text(text)
+        return True
+
+    def ok(self, widget, data=None):
+        """The user has elected to save the changes.
+
+        Called before the dialog returns gtk.RESONSE_OK from run().
+        """
+        pass
+
+    def cancel(self, widget, data=None):
+        """The user has elected cancel changes.
+
+        Called before the dialog returns gtk.RESPONSE_CANCEL for run()
+        """
+        pass
+
+
+if __name__ == "__main__":
+    dialog = SkiptoDialog()
+    dialog.show()
+    gtk.main()
--- transcribe/helpers.py
+++ transcribe/helpers.py
@@ -21,7 +21,10 @@ __all__ = [
     ]
 
 import os
+import datetime
+
 import gtk
+import gio
 
 from transcribe.transcribeconfig import get_data_file
 
@@ -45,3 +48,61 @@ def get_builder(builder_file_name):
     builder.set_translation_domain('transcribe')
     builder.add_from_file(ui_filename)
     return builder
+
+def trim(timestring, digits=1):
+    """
+    Trim a time string to contain only a given number of digits
+    
+    """
+    pos = timestring.find('.')
+    if pos > -1:
+        return timestring[:pos+1+digits]
+    else:
+        return timestring
+
+def ns_to_time(ns):
+    """
+    Converts nanoseconds to a datetime.time object.
+    
+    :Parameters:
+        - `ns`: Nanoseconds as int.
+    
+    :Return:
+        - A datetime.time object.
+        
+    """
+    h = ns / (60 * 60 * 1000000000)
+    mod = ns % (60 * 60 * 1000000000)
+    m = mod / (60 * 1000000000)
+    mod = mod % (60 * 1000000000)
+    s = mod / 1000000000
+    mod = mod % 1000000000
+    ms = mod / 1000
+    return datetime.time(h, m, s, ms)
+    
+def time_to_ns(time):
+    """
+    Converts a datetime.time object to nanoseconds.
+    
+    :Parameters:
+        - `time`: A datetime.time object.
+    
+    :Return:
+        - Nanoseconds as int.
+        
+    """
+    values = []
+    values.append(time.hour * 60 * 60 * 1000000000)
+    values.append(time.minute * 60 * 1000000000)
+    values.append(time.second * 1000000000)
+    values.append(time.microsecond * 1000)
+    return sum(values)
+    
+def filepath_to_uri(path):
+    file = gio.File(path=path)
+    return file.get_uri()
+
+def uri_to_filepath(uri):
+    file = gio.File(uri=uri)
+    return file.get_path()
+
